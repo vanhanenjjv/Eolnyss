e2308c875425c846dd51c131e114a6f0136a6e0a
Janne Vanhanen <vanhanenjjv@outlook.com>
2018-05-28 19:25:26 +0300

    Initial commit

---
+++ src/Eolnyss.Core/GameObject.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eolnyss.Core
+{
+    public abstract class GameObject
+    {
+        public abstract void Update(GameTime gameTime);
+    }
+}
---
+++ src/Eolnyss.Physics/Class1.cs
@@ -0,0 +1,14 @@
+﻿using Eolnyss.Core;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eolnyss.Physics
+{
+    public abstract class PhysicsObject : GameObject
+    { 
+    }
+}
